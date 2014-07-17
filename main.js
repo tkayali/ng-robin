9bf8e08875a773f58453528230855016adf2d72d
tkayali <tkkayali@gmail.com>
2014-07-17 18:27:40 -0400

    assignment 4

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-var app = angular.module('RobinApp', []);
+var app = angular.module('RobinApp', ['ngRoute']);
 
-app.config(['$httpProvider', function($httpProvider) {
+app.config(['$httpProvider', '$routeProvider', function($httpProvider, $routeProvider) {
     $httpProvider.defaults.withCredentials = true;
 }]);
 
-app.controller('RobinController', ['robinUser', 'robinHttp', 'robinMessage', function(robinUser, robinHttp, robinMessage) {
+app.controller('RobinController', ['robinUser', 'robinHttp', 'robinMessage', 'robinFilter', 
+function(robinUser, robinHttp, robinMessage, robinFilter) {
 	this.greeting = 'Hello';
 	this.onLogin = true;
 	this.user = robinUser;
 	this.flashMessage = robinMessage;
+	this.filter = robinFilter;
 
 	this.togglePage = function() {
 		this.onLogin = !this.onLogin;
@@ -53,17 +55,24 @@ app.controller('RobinController', ['robinUser', 'robinHttp', 'robinMessage', fun
 				robinMessage.displayMessage('There was an error :(', true);
 			})
 	};
+
+	this.changeFilter = function(newValue) {
+		this.filter.filter = newValue;
+	}
 }]);
 
-app.controller('RobinListController', ['$interval', 'robinsList', 'robinUser', 'robinHttp', function($interval, robinsList, robinUser, robinHttp) {
+app.controller('RobinListController', ['$interval', 'robinsList', 'robinUser', 'robinHttp', 'robinMessage',
+function($interval, robinsList, robinUser, robinHttp, robinMessage) {
 	this.robinsService = robinsList;
 	this.user = robinUser;
+	this.limit = Infinity;
 
 	this.toggleRead = function(robin) {
-		robinHttp.markAsRead(id)
-			.success(angular.bind(function() {
+		robinHttp.markAsRead(robin._id)
+			.success(function() {
+				console.warn(robin);
 				robin.read = !robin.read;
-			}))
+			})
 			.error(function() {
 				robinMessage.displayMessage('There was an error :(', true);
 			});
@@ -100,7 +109,13 @@ app.factory('robinsList', ['robinHttp', function(robinHttp) {
 		},
 
 		addNewRobin: function(newRobin) {
-			robins.push(newRobin);
+			return robinHttp.createNewRobin(newRobin.message)
+				.success(angular.bind(this, function(robin) {
+					this.robins.unshift(robin);
+				}))
+				.error(function() {
+					console.log(oops)
+				})
 		}
 	};
 }]);
@@ -115,17 +130,21 @@ app.factory('robinUser', function() {
 app.directive('robinForm', function() {
 	return {
 		restrict: 'E',
-		templateUrl: 'robin-form.html'
+		templateUrl: 'robin-form.html',
+		scope: {
+			createNewRobin: '&',
+			theLabel: '@'
+		}
 	};
 });
 
 app.directive('robinChirp', function() {
 	return {
 		restrict: 'E',
+		transclude: true,
 		templateUrl: 'robin-chirp.html',
 		scope: {
 			robin: '=',
-			toggleRead: '&'
 		},
 
 		link: function(scope, el, attrs) {
@@ -142,7 +161,14 @@ app.directive('robinChirp', function() {
 	}
 });
 
-app.service('robinMessage', ['$timeout', function($timeout) {
+app.directive('robinTab', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'robin-tabs.html',
+	}
+});
+
+app.factory('robinMessage', ['$timeout', function($timeout) {
 	return {
 		message: {
 			text: '',
@@ -169,7 +195,7 @@ app.service('robinMessage', ['$timeout', function($timeout) {
 	}
 }]);
 
-app.service('robinHttp', ['$http', function($http) {
+app.factory('robinHttp', ['$http', function($http) {
 	var makeRequest = function(method, resource, data) {
 		var data = data || {},
 			url = 'http://cryptic-hamlet-4443.herokuapp.com';
@@ -211,6 +237,20 @@ app.service('robinHttp', ['$http', function($http) {
 
 		markAsRead: function(id) {
 			return makeRequest('PATCH', '/robins/' + id + '/read');
+		},
+
+		createNewRobin: function(message) {
+			var data = {
+				message: message
+			};
+
+			return makeRequest('POST', '/robins', data)
 		}
 	}
 }]);
+
+app.factory('robinFilter', function() {
+	return {
+		filter: 'unread'
+	}
+});
